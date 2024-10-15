a06b930121a94ec9628f3f0eafe3cdda0e66c6eb
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2024-10-15 09:31:01 -0500

    nuevo build

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store",".nojekyll","assets/.DS_Store","assets/escalera.svg","assets/maletin.svg","assets/matrioska.jpeg","favicon.png"]),
 	mimeTypes: {".svg":"image/svg+xml",".jpeg":"image/jpeg",".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.4eeda2db.js","app":"_app/immutable/entry/app.e0a340d3.js","imports":["_app/immutable/entry/start.4eeda2db.js","_app/immutable/chunks/index.cb6fbcaa.js","_app/immutable/chunks/singletons.3bf455ad.js","_app/immutable/chunks/index.a06bb9a4.js","_app/immutable/entry/app.e0a340d3.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/index.cb6fbcaa.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.a60748ab.js","app":"_app/immutable/entry/app.74ab585c.js","imports":["_app/immutable/entry/start.a60748ab.js","_app/immutable/chunks/index.cb6fbcaa.js","_app/immutable/chunks/singletons.151e968f.js","_app/immutable/chunks/index.a06bb9a4.js","_app/immutable/entry/app.74ab585c.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/index.cb6fbcaa.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
